8d4c7e5e224dc0ef50e4fd9fffaec79208208950
Maria Agustina Santucho <asantucho35@gmail.com>
2023-01-17 23:38:01 -0300

    limpie imagenes. descargue iconos. sigo trabajando en codigo js de favoritos

--- script/favorites.js
+++ script/favorites.js
@@ -1,30 +1,28 @@
 // Agregar a favoritos
 
 const favContainer = document.querySelector(".favorites-container")
-
-
 let favorites = []
 
 function myFavs (array) {
+    console.log(array)
     const favButton = document.querySelectorAll(".button-fav")
     favButton.forEach (button => {
         button.onclick = () => {
             const favId = button.id.slice(7)
-            const filterFavs = array.find ((favorite) => {
+            console.log(favId)
+            const findFavs = array.find ((favorite) => {
                 return favorite.id === Number(favId)
             })
-            favorites.push(filterFavs)
-            saveInLocalStorage("favorites", JSON.stringify(favorites))
+            favorites.push(findFavs)
+            console.log(findFavs)
+            saveInLocalStorage("favorites", favorites)
             }
         })
     }
 
-
-// la ejecución de esta función está en el then del fetch correspondiente, en archivo dom-api.js, como myFavs (data.results) para acceder a la info que trae el fetch
-
-const favoritesList = JSON.parse(getFromLocalStorage("favorites"))
-
-console.log(favoritesList)
-
+const favoritesList = getFromLocalStorage("favorites")
 favorites = favoritesList || []
 
+localStorage.removeItem("favorites")
+
+// la ejecución de esta función está en el then del fetch correspondiente, en archivo dom-api.js, como myFavs(data.results) para acceder a la info que trae el fetch
